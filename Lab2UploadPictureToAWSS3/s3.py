fb7e03da550ce426a4631bae054ff5bb499d3571
Ben <ben.f@pg.com>
2020-01-08 18:39:56 +0800

    offical lab #1

--- Lab2UploadPictureToAWSS3/s3.py
+++ Lab2UploadPictureToAWSS3/s3.py
@@ -15,9 +15,10 @@ def s3_init(region, access_key, secret_key):
     return s3
 
  
-def s3_upload(filepath, group, name, client):
+def s3_upload(filepath, group, client):
     now_time = datetime.datetime.now()
     year = now_time.strftime("%Y")
     month = now_time.strftime("%m")
     date = now_time.strftime("%d")
+    name = filepath
     response = client.upload_file(filepath, 'pgiot-s3-test', constant1 + group + '/' + year + '/' + month + '/' + date + '/' + name, ExtraArgs={'ACL': 'public-read'})
--- MCU.py
+++ MCU.py
@@ -1,12 +1,12 @@
 import time
-import Lab3UploadPictureToAWSS3.s3
-import Lab2AWSIoTconnectivity.connectivity
-import Lab1EdgeFaceDetection.facedetection
+import Lab2UploadPictureToAWSS3.s3 as s3server
+import Lab3AWSIoTconnectivity.connectivity as IoTcore
+import Lab1EdgeFaceDetection.facedetection as faceDetect
 
 ############ step 1 Start your show!!! face detection -----------I am beauty sperator-----
 def initialize_facedetection():
     global faceDetectAlgorithm, video_stream
-    faceDetectAlgorithm, video_stream = Lab1EdgeFaceDetection.facedetection.initialize_facedetection()
+    faceDetectAlgorithm, video_stream = faceDetect.initialize_facedetection()
     print("initiallize facedetection OK")
     
 def facedetection_loop():
@@ -14,8 +14,8 @@ def facedetection_loop():
     result = False
     image_name = 'None'
     
-### Your Beautyful work should be under
-    result,image_name = Lab1EdgeFaceDetection.facedetection.face_detection_loop(
+### Your Beautyful work should be under!!!!!!!
+    result,image_name = faceDetect.face_detection_loop(
         'your face detection algorithm put here',
         'your video stream should be here'
     )
@@ -29,16 +29,25 @@ def facedetection_loop():
 ############ step 2 try to upload your detected image to S3 server
 def initialize_s3():
     global s3_client
-### Put your code down
-   
     
-### Put your code up
+    
+### Your Beautyful work should be under!!!!!!!
+    region = 'put your region name here'
+    access_key = 'put your access_key here'
+    secret_key = 'put your secret_key here'
+    s3_client = s3server.s3_init(region, access_key, secret_key)
+### Do not cross over me, thanks!!!!!!!!!!!!!! 
+
+
     print("initiallize s3 connectivity done")  
     
 def TransmitImageData():
-### Put your code down
+### Your Beautyful work should be under!!!!!!!
+    imageName = 'the image you captured in step 1'
+    groupid = 'which group you are in??'
+    s3server.s3_upload(imageName, group, s3_client)
 
-### Put your code up
+### Do not cross over me, thanks!!!!!!!!!!!!!!
     print("Transmit image data done")
 
 ############# step 2 DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -46,15 +55,37 @@ def TransmitImageData():
 ############ step 3 try to upload your IoT data(your group,you name ...) to IoT core server
 def initialize_connectivity():
     global iot_core_client
-### Put your code down
     
-### Put your code up
+### Your Beautyful work should be under!!!!!!!
+    endpoint = 'put your endpoint here'
+    port = 'put your port number here'
+    root_cert= 'put your root certification location here'
+    private_key= 'put your private key here'
+    device_cert= 'put your device certification here'
+    iot_core_client = IoTcore.IoT_core_init(endpoint,port,root_cert,private_key,device_cert)
+### Do not cross over me, thanks!!!!!!!!!!!!!!
+
     print("initiallize connectivity done")
     
 def TransmitIoTData():
-### Put your code down
+    mytopicheader = "pg/efd/d-1a2d3a2ea4f84eb6b8197a3a3e4a6ad8:2eaa3bcbce2d49f0adf7b3edc3cc4638:sn001/data"
 
-### Put your code up
+### Your Beautyful work should be under!!!!!!!
+    groupid = 'which group you are in??'
+    group_member_count = 'How many members in your group'
+    member_names = 'you name, you team member 1 name, ...'
+    blessing = 'what do you want to say, be careful, everyone can see ^_^'
+
+    transmit_message_to_cloud(mytopicheader, 
+        image_name, 
+        groupid, 
+        group_member_count, 
+        member_names,
+        blessing, 
+        iot_core_client
+    )
+### Do not cross over me, thanks!!!!!!!!!!!!!!
+
     print("transmit IoT Data not OK")
 
 ############# step 3 DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
